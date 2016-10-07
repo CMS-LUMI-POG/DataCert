bdd77024eb7ea7044f647e104c3a125c70cc3e95
capalmer85 <capalmer@cern.ch>
2016-10-07 15:48:42 +0200

    add source for root; update compare script

--- compareTwoCSVsFromBRILCALC.py
+++ compareTwoCSVsFromBRILCALC.py
@@ -2,6 +2,8 @@ import sys
 import ROOT
 
 
+#example call to brilcalc
+#brilcalc lumi --normtag=pccLUM15001 -i /afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/Cert_271036-274443_13TeV_PromptReco_Collisions16_JSON_MuonPhys.txt -u 'hz/ub' -o pcc.csv --output-style=csv --byls --tssec
 
 def dictFromCSVFile(file):
     lines=file.readlines()
@@ -24,8 +26,26 @@ def dictFromCSVFile(file):
 
 filename1=sys.argv[1]
 filename2=sys.argv[2]
+nBXfileName=sys.argv[3]
+filelabel=str(sys.argv[4])
 
-nLS=10
+NBXPerFill={}
+nbxfile=open(nBXfileName)
+for line in nbxfile.readlines():
+    items=line.split(",")
+    try:
+        #run=int(items[0])
+        fill=int(items[0])
+        NBX=int(items[1])
+        NBXPerFill[fill]=NBX
+    except:
+        print "Problem with line",line
+
+nbxfile.close()
+
+nBX=1
+
+time0=0
 
 file1=open(filename1)
 file2=open(filename2)
@@ -33,35 +53,38 @@ file2=open(filename2)
 dict1=dictFromCSVFile(file1)
 dict2=dictFromCSVFile(file2)
 
+total1=0
+total2=0
+
 overtlapKeys=list(set(dict1).intersection(dict2))
 overtlapKeys.sort()
 
 print len(dict1),len(dict2),len(overtlapKeys)
 
+label=filename2.split("_")[0]+"/"+filename1.split("_")[0]
+
 can=ROOT.TCanvas("can","",700,700)
-ratio=ROOT.TH1F("ratio",";ratio "+filename2+"/"+filename1+";",200,0,2)
-ratioNarrow=ROOT.TH1F("ratioNarrow",";ratio "+filename2+"/"+filename1+";",200,0.9,1.1)
+ratio=ROOT.TH1F("ratio",";ratio "+label+";",200,0,2)
+ratioNarrow=ROOT.TH1F("ratioNarrow",";ratio "+label+";",200,0.96,1.04)
 ratioVsTime=ROOT.TGraph()
 ratioVsInst=ROOT.TGraph()
+ratioVsInstProfile=ROOT.TProfile("ratioVsInstProfile",";Average Inst. Luminosity (Hz/#muB);Average "+label,50,0,0.3,0.5,1.5)
+ratioVsTime.SetTitle(";Time (s);"+label)
+ratioVsInst.SetTitle(";Average Inst. Luminosity (Hz/#muB);"+label)
 
 baddies={}
-iCount=0
+iCount=1
+iBin=0
 num=0
 den=0
+combineNLS=15
 for key in overtlapKeys:
     try:
-        num=num+dict2[key][1]
-        den=den+dict1[key][1]
-        if iCount%10==9:
-            ratio.Fill(num/den)
-            ratioNarrow.Fill(num/den)
-            ratioVsTime.SetPoint(iCount,dict1[key][0],num/den)
-            ratioVsInst.SetPoint(iCount,den/10,num/den)
-            num=0
-            den=0
-            
-        iCount=iCount+1
-        if abs(1-dict1[key][1]/dict2[key][1])>.03:
+        if iCount==1:
+            time0=dict1[key][0]
+        #if dict2[key][1]<1000 or dict1[key][1]<1000:
+        #    continue
+        if abs(dict1[key][1]/dict2[key][1]-1)>.2:
             #print key,dict1[key],dict2[key],1-dict2[key]/dict1[key]
             newKey=(key[0],key[1])
             print key
@@ -69,10 +92,28 @@ for key in overtlapKeys:
                 baddies[newKey]=1
             else:
                 baddies[newKey]=baddies[newKey]+1
+            continue
+        num=num+dict2[key][1]
+        den=den+dict1[key][1]
+        total2=total2+dict2[key][1]
+        total1=total1+dict1[key][1]
+        if iCount%combineNLS==0:
+            ratio.Fill(num/den)
+            ratioNarrow.Fill(num/den)
+            ratioVsTime.SetPoint(iBin,dict1[key][0]-time0,num/den)
+            ratioVsInst.SetPoint(iBin,den/NBXPerFill[key[0]]/combineNLS,num/den)
+            ratioVsInstProfile.Fill(den/NBXPerFill[key[0]]/combineNLS,num/den)
+            num=0
+            den=0
+            iBin=iBin+1
+            
+        iCount=iCount+1
 
     except:
         print "fail",key
         pass
+
+print "Totals",total1*23.31e-3,total2*23.31e-3, total2/total1
 
 badkeys=baddies.keys()
 badkeys.sort()
@@ -80,16 +121,23 @@ for key in badkeys:
     if  baddies[key]>10:
         print key, baddies[key]
 
+print "my label",label
+
 ratioVsInst.Draw("AP")
 can.Update()
-raw_input()
+can.SaveAs(filelabel+"_vsInstLumi.png")
+ratioVsInstProfile.SetLineColor(ROOT.kRed)
+ratioVsInstProfile.Draw()
+ratioVsInstProfile.GetYaxis().SetRangeUser(0.95,1.08)
+ratioVsInstProfile.Fit("pol1")
+can.Update()
+can.SaveAs(filelabel+"_vsInstLumiPro.png")
 ratio.Draw()
 can.Update()
-raw_input()
 ratioNarrow.Draw()
-ratioNarrow.Fit("gaus")
+#ratioNarrow.Fit("gaus")
 can.Update()
-raw_input()
+can.SaveAs(filelabel+"_binnedRatio.png")
 ratioVsTime.Draw("AP")
 can.Update()
-raw_input()
+can.SaveAs(filelabel+"_vsTime.png")
